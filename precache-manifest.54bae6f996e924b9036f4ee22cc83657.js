1e50f8ee2c82e93a526a405cee2e7bad4611ecc6
AnastasiyaYutsevych <56453860+AnastasiyaYutsevych@users.noreply.github.com>
2020-06-08 08:13:12 -0400

    Updates

---
+++ precache-manifest.54bae6f996e924b9036f4ee22cc83657.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e687e0ff3122c857916b1afa3cbe474",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "45434d7601488481316e",
+    "url": "/notes/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "35b7ceb283a61bc0c913",
+    "url": "/notes/static/css/main.7444d968.chunk.css"
+  },
+  {
+    "revision": "45434d7601488481316e",
+    "url": "/notes/static/js/2.ce629b66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/notes/static/js/2.ce629b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35b7ceb283a61bc0c913",
+    "url": "/notes/static/js/main.8b9fca33.chunk.js"
+  },
+  {
+    "revision": "0d6a9885c7d29f210fbd",
+    "url": "/notes/static/js/runtime-main.a5dfc2a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/notes/static/media/logo.5d5d9eef.svg"
+  }
+]);
